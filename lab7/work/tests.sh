cad23316cbb48b3708585c049cffd7708a66684c
Will McNeill <wtmcneill@gmail.com>
2012-04-12 15:25:41 -0600

    asdf

--- lab7/work/tests.sh
+++ lab7/work/tests.sh
@@ -5,6 +5,11 @@ cd /home/mcneillw/HPC/lab7/work
 rm -f results.txt
 touch results.txt
 
+. /curc/tools/utils/dkinit #so we can use the "reuse" command
+reuse ICS
+
+make
+
 for i in 2 4 8 12; do
 	export OMP_NUM_THREADS=$i
 	./omp_MatVecMultiply -m 80000 -n 8000 >> results.txt
