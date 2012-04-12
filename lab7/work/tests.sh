673e89697b130e60aa010914fd6323edc9a431c7
Will McNeill <wtmcneill@gmail.com>
2012-04-12 15:32:26 -0600

    asfd

--- lab7/work/tests.sh
+++ lab7/work/tests.sh
@@ -6,11 +6,12 @@ rm -f results.txt
 touch results.txt
 
 . /curc/tools/utils/dkinit #so we can use the "reuse" command
-reuse ICS
+reuse ICS #otherwise cant find some omp shared object
 
 make
 
 for i in 1 2 4 8 12; do
 	export OMP_NUM_THREADS=$i
+	echo running on $i threads
 	./omp_MatVecMultiply -m 80000 -n 8000 >> results.txt
 done
