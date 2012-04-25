cdaaf3bb395a1f1c928f9e4ea82f1cbf120cecbd
Will McNeill <wtmcneill@gmail.com>
2012-04-25 14:42:41 -0600

    lab9

--- lab9/compile.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-cd /home/mcneillw/HPC/lab8/
-
-. /curc/tools/utils/dkinit #so we can use the "reuse" command
-reuse .pgi-12.3-testing
-
-pgcc -ta=nvidia -Minfo -o jacobiSS jacobiSS.c
--- lab9/run.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-cd /home/mcneillw/HPC/lab8/
-
-./compile.sh
-
-for i in 100 200 400 800 1600 3200; do
-	./jacobiSS $i
-done
